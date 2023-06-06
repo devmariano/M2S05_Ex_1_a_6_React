6bbf5d17a6c0dfb29ce573a25351230829c4010b
devmariano <developer.mariano@gmail.com>
2023-06-06 20:36:43 -0300

    [M2S05] Ex 4 - Novos componentes

--- Exercicio_4/exercicio-react/src/components/Button.js
+++ Exercicio_4/exercicio-react/src/components/Button.js
@@ -6,7 +6,7 @@ import React from "react";
   function Button() {
     return  (
     <React.Fragment>
-      <button className="Btn" type="submit">'Enviar'</button>
+      <button className="Btn" type="submit">Enviar</button>
     </React.Fragment>
     );
   }
